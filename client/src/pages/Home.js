7fcb36c0e9f851662ffbd952ab0bbe3fa212c47f
Adam Menkes <amenkes24@gmail.com>
2022-02-27 13:12:15 -0800

    create connection between front end and back end

---
+++ client/src/pages/Home.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import { useQuery } from '@apollo/client';
+import { QUERY_REVIEWS } from '../utils/queries';
+
+import ReviewList from '../components/ReviewList';
+
+const Home = () => {
+  // use useQuery hook to make query request
+  const { loading, data } = useQuery(QUERY_REVIEWS);
+  const reviews = data?.reviews || [];
+  console.log(reviews);
+
+  return (
+    <main>
+  <div className="flex-row justify-space-between">
+    <div className="col-12 mb-3">
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        <ReviewList reviews={reviews} title="Let the movie discussions... Begin!" />
+      )}
+    </div>
+  </div>
+</main>
+  );
+};
+
+export default Home;
