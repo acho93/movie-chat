d01cdd3e9f404f073e4375120e5805dc27e22baa
Adam Menkes <amenkes24@gmail.com>
2022-03-01 14:53:31 -0800

    create user based queries

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,26 +1,43 @@
 import React from 'react';
 
 import { useQuery } from '@apollo/client';
-import { QUERY_REVIEWS } from '../utils/queries';
+import { QUERY_REVIEWS, QUERY_ME_BASIC } from '../utils/queries';
+
+import Auth from '../utils/auth';
 
 import ReviewList from '../components/ReviewList';
+import FriendList from '../components/FriendList';
 
 const Home = () => {
+
+  const loggedIn = Auth.loggedIn();
+
   // use useQuery hook to make query request
   const { loading, data } = useQuery(QUERY_REVIEWS);
+  // use object destructuring to extract `data` from the `useQuery` Hook's response and rename it `userData` to be more descriptive
+  const { data: userData } = useQuery(QUERY_ME_BASIC);
   const reviews = data?.reviews || [];
   console.log(reviews);
 
   return (
     <main>
   <div className="flex-row justify-space-between">
-    <div className="col-12 mb-3">
+    <div className={`col-12 mb-3 ${loggedIn && 'col-lg-8'}`}>
       {loading ? (
         <div>Loading...</div>
       ) : (
         <ReviewList reviews={reviews} title="Let the movie discussions... Begin!" />
       )}
     </div>
+    {loggedIn && userData ? (
+      <div className="col-12 col-lg-3 mb-3">
+        <FriendList
+          username={userData.me.username}
+          friendCount={userData.me.friendCount}
+          friends={userData.me.friends}
+        />
+      </div>
+    ) : null}
   </div>
 </main>
   );
