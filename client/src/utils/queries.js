92f1efc9a8213ad8da98a69163f117da248ad0c4
Adam Menkes <amenkes24@gmail.com>
2022-03-01 12:41:37 -0800

    build out profile page

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -35,3 +35,24 @@ export const QUERY_REVIEW = gql`
     }
   }
 `;
+
+export const QUERY_USER = gql`
+  query user($username: String!) {
+    user(username: $username) {
+      _id
+      username
+      email
+      friendCount
+      friends {
+        _id
+        username
+      }
+      reviews {
+        _id
+        reviewText
+        createdAt
+        reactionCount
+      }
+    }
+  }
+`;
