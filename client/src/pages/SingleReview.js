4683afb8a68a129ce9318bc3d443c6292aa6aa0a
Adam Menkes <amenkes24@gmail.com>
2022-03-01 12:21:03 -0800

    create single review route

--- client/src/pages/SingleReview.js
+++ client/src/pages/SingleReview.js
@@ -1,19 +1,38 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+
+import { useQuery } from '@apollo/client';
+import { QUERY_REVIEW } from '../utils/queries';
 
 const SingleReview = props => {
+
+  const { id: reviewId } = useParams();
+  
+  const { loading, data } = useQuery(QUERY_REVIEW, {
+    variables: { id: reviewId }
+  });
+
+  const review = data?.review || {};
+
+  if (loading) {
+    return <div>Loading...</div>
+  }
+
   return (
     <div>
+     <div>
       <div className="card mb-3">
         <p className="card-header">
           <span style={{ fontWeight: 700 }} className="text-light">
-            Username
+            {review.username}
           </span>{' '}
-          thought on createdAt
+          review on {review.createdAt}
         </p>
         <div className="card-body">
-          <p>Thought Text</p>
+          <p>{review.reviewText}</p>
         </div>
       </div>
+    </div>
     </div>
   );
 };
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -17,3 +17,21 @@ export const QUERY_REVIEWS = gql`
         }
     }
 `;
+
+export const QUERY_REVIEW = gql`
+  query review($id: ID!) {
+    review(_id: $id) {
+      _id
+      reviewText
+      createdAt
+      username
+      reactionCount
+      reactions {
+        _id
+        createdAt
+        username
+        reactionBody
+      }
+    }
+  }
+`;
